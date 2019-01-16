3df4d471cca1c67e3f1f6bd53ba99c92dbc14e4e
Harpriya Bagri <harpriyabagri@gmail.com>
2019-01-16 22:37:59 +0800

    Update dealcard.sv

--- dealcard.sv
+++ dealcard.sv
@@ -1,6 +1,3 @@
-// Lucky you! We are giving you this code for free. There is nothing
-// here you need to add or write.
-
 module dealcard(input clock, input resetb, output [3:0] new_card);
 		  
 reg [3:0] dealer_card;

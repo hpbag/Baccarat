b0ac6bec8ca0211a8a2bd7c02b1b8bd7b68a8d82
Harpriya Bagri <harpriyabagri@gmail.com>
2019-01-16 22:38:29 +0800

    spacing

--- scorehand.sv
+++ scorehand.sv
@@ -1,8 +1,5 @@
 module scorehand(input [3:0] card1, input [3:0] card2, input [3:0] card3, output [3:0] total);
 
-// The code describing scorehand will go here.  Remember this is a combinational
-// block. The function is described in the handout.  Be sure to review the section
-// on representing numbers in the lecture notes.
 	reg [3:0] temp1, temp2, temp3;
 	wire [6:0] tempcard1, tempcard2, tempcard3;
 	

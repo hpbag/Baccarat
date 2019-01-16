aa3f5682e6aa105486467ddb5d020feaeb1b721e
Harpriya Bagri <harpriyabagri@gmail.com>
2019-01-16 22:35:58 +0800

    rename

--- baccarat.sv
+++ baccarat.sv
@@ -1,8 +1,4 @@
-// This module contains a Verilog description of the top level module
-// Assuming you don't modify the inputs and outputs of the various submodules,
-// you should not have to modify anything in this file.
-
-module lab1(input CLOCK_50, input[3:0] KEY, output[9:0] LEDR,
+module baccarat(input CLOCK_50, input[3:0] KEY, output[9:0] LEDR,
             output[6:0] HEX5, output[6:0] HEX4, output[6:0] HEX3,
             output[6:0] HEX2, output[6:0] HEX1, output[6:0] HEX0);
 
